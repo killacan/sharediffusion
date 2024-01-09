0064af62094e53c23455c55aca43831da0c0225c
Leisurellama <kameron000@yahoo.com>
2024-01-08 18:39:10 -0600

    get user login working

--- components/AuthButton.tsx
+++ components/AuthButton.tsx
@@ -22,7 +22,7 @@ export default async function AuthButton() {
 
   return user ? (
     <div className="flex items-center gap-4">
-      Hey, {user.email}!
+      Hey, {user.user_metadata.username}!
       <form action={signOut}>
         <button className="py-2 px-4 rounded-md no-underline bg-btn-background hover:bg-btn-background-hover">
           Logout
