902514e5209f3c5f16845c8ee5673621b0632e30
Leisurellama <kameron000@yahoo.com>
2024-01-10 16:39:08 -0600

    add first draft of tos

--- app/_components/footer.tsx
+++ app/_components/footer.tsx
@@ -1,5 +1,7 @@
 import { DiGithubBadge } from "react-icons/di";
 import { FaDiscord } from "react-icons/fa";
+import LinkComponent from "./link";
+import { Link } from "lucide-react";
 
 export default function Footer() {
 
@@ -8,6 +10,8 @@ export default function Footer() {
       <div className="w-full max-w-4xl flex items-center p-3 text-3xl">
         <a className="px-3" href="https://github.com/killacan/sharediffusion" target="_blank" rel="noopener noreferrer"><DiGithubBadge /></a>
         <a className="px-3" href="https://discord.gg/y7aMda893Y" target="_blank" rel="noopener noreferrer"><FaDiscord /></a>
+        <LinkComponent href={"/privacypolicy"}>Privacy Policy</LinkComponent>
+        <LinkComponent href={"/tos"}>Terms and Conditions</LinkComponent>
       </div>
     </footer>
   )
