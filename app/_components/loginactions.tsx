175c7641177590158aa67a28676939a9c4a68074
Leisurellama <kameron000@yahoo.com>
2024-01-16 10:48:28 -0600

    migrate forms with Zod

--- app/_components/loginactions.tsx
+++ app/_components/loginactions.tsx
@@ -3,15 +3,15 @@ import { createClient } from '@/utils/supabase/server'
 import { redirect } from 'next/navigation';
 import { headers, cookies } from 'next/headers'
 import { formSchema } from '../post/page';
+import { signinFormSchema, signupFormSchema } from '../login/page' 
 import { z } from 'zod';
 
 
-export async function signIn(formData: FormData) {
+export async function signIn(values: z.infer<typeof signinFormSchema>) {
     'use server';
 
-    const email = formData.get('email') as string
-    const password = formData.get('password') as string
-    const username = formData.get('username') as string
+    const email = values.email as string
+    const password = values.password as string
     const cookieStore = cookies()
     const supabase = createClient(cookieStore)
 
@@ -28,17 +28,18 @@ export async function signIn(formData: FormData) {
 
 }
 
-export async function signUp(formData: FormData) {
+export async function signUp(values: z.infer<typeof signupFormSchema>) {
   'use server';
 
   const cookieStore = cookies()
   const supabase = createClient(cookieStore)
 
   const origin = headers().get('origin')
-  const email = formData.get('email') as string
-  const password = formData.get('password') as string
-  const username = formData.get('username') as string
-  const { data: user } = await supabase.from('users').select('*').eq('username', username).single()
+  const email = values.email as string
+  const password = values.password as string
+  const username = values.username as string
+  console.log(email, password, username, origin)
+  // const { data: user } = await supabase.from('users').select('*').eq('username', username).single()
 
   const { error } = await supabase.auth.signUp({
     email,
@@ -51,9 +52,9 @@ export async function signUp(formData: FormData) {
     },
   })
 
-  if (user) {
-    return redirect('/login?message=Username already taken')
-  }
+  // if (user) {
+  //   return redirect('/login?message=Username already taken')
+  // }
 
   if (error) {
     console.log(error)
