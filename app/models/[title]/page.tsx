b5306ab17695558be65938234fe23742ee31b11d
Leisurellama <kameron000@yahoo.com>
2024-01-22 18:40:10 -0600

    update nextjs and add version select for Models

--- app/models/[title]/page.tsx
+++ app/models/[title]/page.tsx
@@ -3,10 +3,30 @@ import { createClient } from '@/utils/supabase/server'
 import { headers, cookies } from 'next/headers'
 import Link from 'next/link'
 import { redirect } from 'next/navigation'
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+  } from "../../_components/ui/select"
+import { PostgrestSingleResponse } from '@supabase/postgrest-js'
 
 export const revalidate = 120
 
+interface Versions {
+    error: any,
+    data: [Version]
+}
 
+interface Version {
+    version_id: number,
+    post_id: number,
+    version_magnet: string,
+    upload_date: string,
+    name: string,
+    user_id: string,
+}
 
 export default async function Post({ params: { title } }: { params: { title: string } }) {
 
@@ -24,17 +44,40 @@ export default async function Post({ params: { title } }: { params: { title: str
     if (error) {
         console.log(error)
     }
+    let versions
+    if (userData) {
+        versions = await supabase.from('versions').select('*').eq('post_id', data.post_id)
+    }
 
-    if (!data || !userData || error) {
+    if (!data || !userData || error || !versions) {
         return (
             <div className="flex-1 w-full flex flex-col gap-20 items-center">
                 <div className="animate-in flex-1 flex flex-col gap-3 opacity-0 max-w-4xl px-3">
-                    <h1 className="text-3xl font-bold">Error getting model</h1>
+                    <h1 className="text-3xl font-bold">Error getting model and versions</h1>
                 </div>
             </div>
         )
 
     }
+
+    const selectionBuilder = (versions: PostgrestSingleResponse<any[]>) => {
+        console.log(versions, "this is the version")
+
+
+        if (!versions.data) {
+            return (
+                <SelectItem value="No Versions">No Versions</SelectItem>
+            )
+        }
+
+        let selectionItems = versions.data.map((version: Version) => (
+            <SelectItem value={`${version.name}`}>{version.name}</SelectItem>
+        ))
+
+        return selectionItems
+    }
+
+    // console.log(version)
 
 
     return (
@@ -47,7 +90,14 @@ export default async function Post({ params: { title } }: { params: { title: str
                     <div>
                         <h1 className="text-3xl font-bold">{data.title}</h1>
                         <p>Post</p>
-
+                        {versions.data && <Select defaultValue={`${versions?.data[0].name}`}>
+                        <SelectTrigger className="w-[180px]">
+                            <SelectValue placeholder='Please Select'/>
+                        </SelectTrigger>
+                        <SelectContent>
+                            {selectionBuilder(versions)}
+                        </SelectContent>
+                        </Select>}
                     </div>
 
                 </div>
